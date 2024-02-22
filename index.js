52b88749f37b9dfa2c6805314ff41c06b9923639
revesfideles <revesfideles@gmail.com>
2024-02-22 17:27:42 -0500

    completed lab

---
+++ index.js
@@ -0,0 +1,10 @@
+const divide = function(a, b) {
+    const result = a / b;
+    return result;
+  }
+
+const square = (num) => {
+    return num * num;
+  };
+  
+const add = (num1, num2) => num1 + num2;
